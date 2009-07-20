2aed8847dff9f52472648c72829fc6ec6cd9edff
Michael Reinsch <michael@mobalean.com>
2009-07-20 13:48:37 +0900

    reformatted

--- lib/acts_as_rateable.rb
+++ lib/acts_as_rateable.rb
@@ -4,7 +4,7 @@ module ActiveRecord
       def self.included(base)
         base.extend(ClassMethods)
       end
-      
+
       module ClassMethods
         def acts_as_rateable(options = {})
           has_many :ratings, :as => :rateable, :dependent => :destroy
@@ -16,7 +16,7 @@ module ActiveRecord
           include ActiveRecord::Acts::Rateable::InstanceMethods
         end
       end
-            
+
       module InstanceMethods
         # Rates the object by a given score. A user object should be passed to the method.
         def rate_it(score, user)
@@ -24,7 +24,7 @@ module ActiveRecord
             rating.update_attributes!(:score => score)
           end
         end
-        
+
         # Calculates the average rating. Calculation based on the already given scores.
         def average_rating
           avg = ratings.average(:score)
@@ -35,13 +35,13 @@ module ActiveRecord
         def average_rating_round
           average_rating.round
         end
-    
+
         # Returns the average rating in percent.
         def average_rating_percent
           f = 100 / max_rating.to_f
           average_rating * f
         end
-        
+
         # Checks whether a user rated the object or not.
         def rated_by?(user)
           ratings.exists?(:user_id => user)
@@ -54,7 +54,7 @@ module ActiveRecord
         end
 
       end
-      
+
     end
   end
 end
