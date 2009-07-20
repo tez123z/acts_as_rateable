9a1a35beac6e6aa52ed0f30d4039782c1e6cff96
Michael Reinsch <michael@mobalean.com>
2009-07-20 22:59:58 +0900

    should also take string for score

--- test/lib/acts_as_rateable_test.rb
+++ test/lib/acts_as_rateable_test.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), '../test_helper'))
 class ActsAsRateableTest < ActiveSupport::TestCase
 
   test "rate_it one user" do
-    books(:alice).rate_it(4, users(:john))
+    books(:alice).rate_it("4", users(:john))
     assert_equal 4.0, books(:alice).average_rating
     assert_equal 4, books(:alice).rating_by(users(:john))
     assert_equal true, books(:alice).rated_by?(users(:john))
@@ -12,7 +12,7 @@ class ActsAsRateableTest < ActiveSupport::TestCase
 
   test "rate_it multiple users" do
     books(:alice).rate_it(4, users(:jane))
-    books(:alice).rate_it(5, users(:bill))
+    books(:alice).rate_it("5", users(:bill))
     assert_equal 4.5, books(:alice).average_rating
     assert_equal 4, books(:alice).rating_by(users(:jane))
     assert_equal 5, books(:alice).rating_by(users(:bill))
