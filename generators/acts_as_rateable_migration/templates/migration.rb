674d422e8a78aed052bc1fe6e0d640b4415f9db7
Michael Reinsch <michael@mobalean.com>
2009-07-20 19:30:41 +0900

    use strings for table names, nicer formatting, don't force table creation

--- generators/acts_as_rateable_migration/templates/migration.rb
+++ generators/acts_as_rateable_migration/templates/migration.rb
@@ -1,23 +1,23 @@
 class ActsAsRateableMigration < ActiveRecord::Migration
   def self.up
     create_table "ratings" do |t|
-      t.references "rateable", :polymorphic => true, :nil => false
+      t.references "rateable",       :polymorphic => true, :nil => false
       t.float      "average_rating"
       t.timestamps
     end
     add_index "ratings", ["rateable_id", "rateable_type"]
 
-    create_table "user_ratings", :force => true do |t|
+    create_table "user_ratings" do |t|
       t.references "rating", :nil => false
-      t.references "user", :nil => false
-      t.integer    "score", :nil => false
+      t.references "user",   :nil => false
+      t.integer    "score",  :nil => false
       t.timestamps
     end
     add_index "user_ratings", ["user_id", "rating_id"]
   end
 
   def self.down
-    drop_table :user_ratings
-    drop_table :ratings
+    drop_table "user_ratings"
+    drop_table "ratings"
   end
 end
